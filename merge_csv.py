d1183f7255e3d93d93b3d1bde80b2a5e29b5f8ba
unknown <maxime.nicaise@student-cs.fr>
2025-03-05 11:18:17 +0100

    blaaa

--- merge_csv.py
+++ merge_csv.py
@@ -8,21 +8,12 @@ colonnes_1 = list(set(df1.columns))
 colonnes_2 = list(set(df2.columns))
 
 correspondances = {
-    "code_insee_commune_actualise": "Code_INSEE_(BAN)",
-    "date_reception_dpe": "Date_réception_DPE",
     "surface_utile": "Surface_utile",
     "shon": "Surface_(SHON)",
-    "commune": "Nom__commune_(BAN)",
-    "nom_rue": "Nom__rue_(BAN)",
-    "date_etablissement_dpe": "Date_établissement_DPE",
     "date_visite_diagnostiqueur" : "Date_visite_diagnostiqueur",
     "annee_construction": "Année_construction",
     "secteur_activite": "Secteur_activité",
-    "code_postal": "Code_postal_(brut)",  # il faut voir si c pas mieux de mettre code postal ban
-    "numero_dpe": "N°DPE",
-    "nom_methode_dpe": "Méthode_du_DPE",
-    "tr001_modele_dpe_code": "Modèle_DPE",
-    "tr013_type_erp_categorie": "Catégorie_ERP",
+    #"code_postal": "Code_postal_(brut)",  # il faut voir si c pas mieux de mettre code postal ban
     "classe_estimation_ges" : "Etiquette_GES",
     "estimation_ges" : "Emission_GES_kgCO2/m²/an"
 }
@@ -58,5 +49,5 @@ df_merged = pd.merge(
     right_on=list(correspondances.values()),
     how="outer"
 )
-#print(df_merged.head())
-df_merged.to_csv("fusion.csv", index=False)
+print(df_merged.head())
+#df_merged.to_csv("fusion.csv", index=False)

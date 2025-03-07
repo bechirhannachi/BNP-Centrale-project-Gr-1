b03e535cd355edabc6d23cdab2d4ff87edba82b9
unknown <maxime.nicaise@student-cs.fr>
2025-03-07 16:01:42 +0100

    maj

--- merge_csv.py
+++ merge_csv.py
@@ -28,7 +28,6 @@ correspondances = {
 }
 
 
-
 df2 = df2.rename(columns={v: k for k, v in correspondances.items()})
 
 # Garder uniquement les colonnes du dictionnaire
@@ -42,9 +41,9 @@ for col in colonnes_numeriques:
     df2[col] = pd.to_numeric(df2[col], errors="coerce")
 
 # Convertir les colonnes en types compatibles
-for col in colonnes_finales:
-    df1[col] = pd.to_numeric(df1[col], errors="coerce") if pd.api.types.is_numeric_dtype(df2[col]) else df1[col].astype(str)
-    df2[col] = pd.to_numeric(df2[col], errors="coerce") if pd.api.types.is_numeric_dtype(df1[col]) else df2[col].astype(str)
+# for col in colonnes_finales:
+#     df1[col] = pd.to_numeric(df1[col], errors="coerce") if pd.api.types.is_numeric_dtype(df2[col]) else df1[col].astype(str)
+#     df2[col] = pd.to_numeric(df2[col], errors="coerce") if pd.api.types.is_numeric_dtype(df1[col]) else df2[col].astype(str)
 
 
 
@@ -52,10 +51,10 @@ for col in colonnes_finales:
 df_merged = pd.concat([df1, df2], ignore_index=True)
 
 # Filtrer les valeurs aberrantes
-df_merged = df_merged[df_merged["estimation_ges"] > 0]
-df_merged = df_merged[df_merged["surface_utile"] > 0]
-df_merged = df_merged[df_merged["shon"] > 0]
-df_merged = df_merged[df_merged["consommation_energie"] > 0]
+df_merged = df_merged[df_merged["estimation_ges"] >= 0]
+df_merged = df_merged[df_merged["surface_utile"] >= 0]
+df_merged = df_merged[df_merged["shon"] >= 0]
+df_merged = df_merged[df_merged["consommation_energie"] >= 0]
 
 
 # Sauvegarder le fichier final
@@ -66,10 +65,5 @@ colonnes_finales = list(correspondances.keys())
 
 df_merged = df_merged[colonnes_finales]
 
-
-df_merged = df_merged[df_merged["estimation_ges"] > 0]
-df_merged = df_merged[df_merged["surface_utile"] > 0]
-df_merged = df_merged[df_merged["shon"] > 0]
-df_merged = df_merged[df_merged["consommation_energie"] > 0]
 print(df_merged.head())
 #df_merged.to_csv("fusion.csv", index=False)
